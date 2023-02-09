a9a3cf44ce5aa9dfd2f5e37722dacf3da9be584a
Corentin Cheminaud <corentin.cheminaud@icloud.com>
2023-02-10 00:33:59 +0100

    fix(fish): do not load fnm completions already installed by homebrew

--- .config/fish/conf.d/fnm.fish
+++ .config/fish/conf.d/fnm.fish
@@ -9,15 +9,4 @@ if not status is-interactive; or not is_available fnm
     exit 0
 end
 
-function __fnm_load_completions
-    set -l completions (string split '\n' (fnm completions --shell fish))
-    for i in (seq (count $completions))
-        eval $completions[$i]
-    end
-end
-
-# Load fnm
 fnm env --use-on-cd | source
-
-# Load completions
-__fnm_load_completions
