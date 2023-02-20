a652d3942ba1a0662cf7ae3dfa0fc345860d38da
Corentin Cheminaud <corentin.cheminaud@icloud.com>
2023-02-20 12:05:18 +0100

    feat(fnm): resolve Node version browsing current and parents directories

--- .config/fish/conf.d/fnm.fish
+++ .config/fish/conf.d/fnm.fish
@@ -9,4 +9,7 @@ if not status is-interactive; or not is_available fnm
     exit 0
 end
 
+# Resolving Node version browsing current directory and all parent directories
+set -x FNM_VERSION_FILE_STRATEGY 'recursive'
+
 fnm env --use-on-cd | source
