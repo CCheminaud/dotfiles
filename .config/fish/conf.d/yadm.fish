9fcb5d4959b5aefb7bb0c8bf7dadbfdb13dda08f
Corentin Cheminaud <corentin.cheminaud@icloud.com>
2023-05-19 09:24:47 +0200

    feat(fish): add some abbreviations for YAML

---
+++ .config/fish/conf.d/yadm.fish
@@ -0,0 +1,14 @@
+#
+# ~/.config/fish/conf.d/yadm.fish
+#
+# This file contains everything needed for YADM integration.
+#
+
+if not status is-interactive; or not is_available yadm
+    exit 0
+end
+
+# Abbreviations (based on Git abbreviations)
+abbr -ag yalo 'yadm log --date-order --graph --pretty=custom-full'
+abbr -ag yalol 'yadm log --date-order --graph --pretty=custom-oneline'
+abbr -ag yast 'yadm status'
