fee8c349fcdb2e157f2b747c2b34dbddefee6071
Corentin Cheminaud <corentin.cheminaud@icloud.com>
2024-01-01 23:08:28 +0100

    fix(fish): fix an issue with Fisher plugins not loaded at launch
    When $fish_path is changed:
    - load manually the plugin scripts
    - update fish_complete_path & fish_function_path
    
    Since Fisher is installed with brew, it is removed from plugin list.

--- .config/fish/conf.d/fisher.fish
+++ .config/fish/conf.d/fisher.fish
@@ -9,5 +9,18 @@ if not status is-interactive; or not command_exists fisher
     exit 0
 end
 
-# Set Fisher plugins installation path
+# Set plugins installation path
 set -U fisher_path ~/.config/fish/__plugins
+
+# If installation path is not the default one, Fish will not be able to find the files.
+# We need to indicate where plugin stuff is located (completions, functions...) and scripts must be loaded manually.
+# See https://github.com/jorgebucaran/fisher/issues/640.
+
+if set --query fisher_path
+    set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..]
+    set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..]
+
+    for file in $fisher_path/conf.d/*.fish
+        source $file
+    end
+end
