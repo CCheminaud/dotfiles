71b42359a4924998a4692e3126e60d605fd8e235
Corentin Cheminaud <corentin.cheminaud@icloud.com>
2024-05-20 22:13:28 +0200

    feat(starship): enable transient prompt with fish

--- .config/fish/conf.d/starship.fish
+++ .config/fish/conf.d/starship.fish
@@ -9,4 +9,14 @@ if not status is-interactive; or not command_exists starship
     exit 0
 end
 
+function starship_transient_prompt_func
+    echo
+    starship module character
+end
+
+function starship_transient_rprompt_func
+    starship module time
+end
+
 starship init fish | source
+enable_transience
