0b3fd078c7f089efec9379c10e11a292c65c84ba
Corentin Cheminaud <corentin.cheminaud@icloud.com>
2024-07-05 11:43:47 +0200

    fix: remove right prompt for starship transient mode

--- .config/fish/conf.d/starship.fish
+++ .config/fish/conf.d/starship.fish
@@ -14,9 +14,5 @@ function starship_transient_prompt_func
     starship module character
 end
 
-function starship_transient_rprompt_func
-    starship module time
-end
-
 starship init fish | source
 enable_transience
